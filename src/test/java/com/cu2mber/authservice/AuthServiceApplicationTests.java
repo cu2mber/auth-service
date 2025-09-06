7be3ef81db62377b4d4da62aafe0e4382965f29c
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2025-09-07 00:22:59 +0900

    Created Maven project for auth-service

---
+++ src/test/java/com/cu2mber/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu2mber.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
